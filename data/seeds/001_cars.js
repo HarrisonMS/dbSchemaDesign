a7b9872f04aac16d48602115c4d1d38e34b6eb3f
Harrison <hmseaborn@gmail.com>
2020-05-12 12:47:54 -0700

    messing arround with the faker api

--- data/seeds/001_cars.js
+++ data/seeds/001_cars.js
@@ -8,12 +8,12 @@ exports.seed = function (knex, promise) {
       for (let index = 0; index < dataLength; index++) {
         cars.push({
           VIN: faker.random.uuid(),
-          make: faker.lorem.word(),
-          model: faker.hacker.phrase(),
-          year: faker.date.past(),
-          mileage: faker.random.number(),
+          make: faker.random.word(),
+          model: faker.hacker.noun(),
+          year: faker.random.number({ min: 1960, max: 2020 }),
+          mileage: faker.random.number({ min: 1, max: 250000 }),
           transmission: faker.hacker.verb(),
-          title_status: faker.hacker.phrase(),
+          title_status: faker.hacker.noun(),
         });
       }
       return knex("cars").insert(cars);

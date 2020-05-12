81dad64b7295b37f0dbcf8b79b8621a564e6416c
Harrison <hmseaborn@gmail.com>
2020-05-12 12:13:31 -0700

    knexfile created

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const server = require("./api/server");
 
 const port = process.env.PORT || 5432;
+
 server.listen(port, () => {
   console.log(`\n== API running on port ${port} ==\n`);
 });
-
-// server.listen(PORT, () => {
-//   console.log(`\n== API running on port ${PORT} ==\n`);
-// });
